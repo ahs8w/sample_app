66469d6a67eba9708ed71f65002a0ba2eb668c7f
Adam Schiller <ahs8w@virginia.edu>
2013-08-01 20:35:18 +0200

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # establishing a default title if one is not set with 'provide' in the view
+  def full_title(page_title)
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
