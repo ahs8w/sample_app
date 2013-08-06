8112d02c3535442d1962d236bf1d5c2e34beaad6
emahs8w/em <emahs8w@virginia.edu/em>
2013-08-06 16:15:56 +0200

    chp 6 exercises: update regex and testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }   # callback: mitigates risk of case-sensitivity in databases
+  before_save { email.downcase! }             # callback: mitigates risk of case-sensitivity in databases
+  # same as ->  before_save { self.email = email.downcase }
 
   validates :name,  presence: true, length: { maximum: 50 }
     # same as validates(:name, presence: true)  ->  method
     # can test in console with user.valid?
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i   # regex constant(capital letter): value cannot change
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+    # regex constant(starts w/ capital letter): value cannot change
   validates :email, presence: true, 
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }  # default is case-sensitive we want case-insensitive -> false
@@ -23,7 +25,7 @@ end
 
 
 
-# VALID_EMAIL_REGEX                     www.rubular.org
+# VALID_EMAIL_REGEX (simple)            www.rubular.org
 
 # /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  full regex
 # /                                     start of regex
@@ -36,3 +38,10 @@ end
 # \z                                    match end of a string
 # /                                     end of regex
 # i                                     case insensitive
+
+# VALID_EMAIL_REGEX (stronger)
+# /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+# [a-z\d\-]+                            at least one letter, digit, hyphen -> no dot)
+# (\.[a-z]+)*                           dot + at least one character -> allows dot in domain name but not ..com
+# \.                                    literal dot after domain name
+# [a-z]+                                at least one letter                            
